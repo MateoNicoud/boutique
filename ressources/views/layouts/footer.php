538254cee2dcb2c7c23fbe547cebb7627da3f6ba
thouvenot <thouvenot@thouvenot-desktop>
2024-02-07 14:18:09 +0100

    layout

---
+++ ressources/views/layouts/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/?action=cart">Cart</a>
+            </li>
+        </ul>
+    </nav>
+</footer>
---
+++ ressources/views/layouts/header.php
@@ -0,0 +1,21 @@
+<header data-bs-theme="dark">
+    <div class="collapse text-bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4>About</h4>
+                    <p class="text-body-secondary">produit</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+         <h3 class="text-white">MENU</h3>
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                    aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
