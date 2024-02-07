4c869ac905877d262b98898247c365511b9e0946
thouvenot <thouvenot@thouvenot-desktop>
2024-02-07 14:19:40 +0100

    web (routes)

--- route/web.php
+++ route/web.php
@@ -1,1 +1,26 @@
 <?php
+// #votre FrontController avec les routes HTTP
+
+// Récupère le paramètre 'action' de la requête GET
+$action = filter_input(INPUT_GET, "action");
+
+// Si 'action' est vide, le définir comme '/'
+if (empty($action)) {
+    $action = '/';
+}
+
+// Définit un tableau de routes associant les actions aux chemins de fichiers correspondants
+$routes = [
+    'product' => "../app/controllers/showProductController.php",
+    'cart'=> "../app/controllers/cartController.php",
+    '/' => "../app/controllers/homeController.php",
+];
+
+// Vérifie si l'action spécifiée existe dans le tableau des routes
+if (isset($routes[$action])) {
+    // Si l'action existe, inclut le fichier correspondant
+    include $routes[$action];
+}
+
+?>
+
